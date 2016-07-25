cb31b0c2422db825c0680ad0cd6d07d0ff67aa7b
Evin Sellin <evinism@gmail.com>
2016-07-24 17:34:58 -0700

    Make a little cleaner

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
-// webpack (aka our task-runner) knows to look for index inside of the App folder.
-// Then the 'export default' slaps it right in there!
 import App from './App';
 
 $(document).ready(() => {
-  // See the thing I'm passing into the "App" tag as if it were html? That's a prop!
-  // Notice also that App is capitalized. That's meaningful, unfortunately.
-  // If something's a react component, it should start with a capital letter
-  ReactDOM.render(<App nameOfFriend="Leo" />, $('#app-container')[0]);
+  ReactDOM.render(<App />, $('#react-mount')[0]);
 });
